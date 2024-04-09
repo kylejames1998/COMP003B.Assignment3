84a08338c0a9eb401b8647b8f7fc698adf954066
Kyle James <140095432+kylejames1998@users.noreply.github.com>
2024-04-09 08:39:11 -0700

    added controller logic

---
+++ COMP003B.Assignment3/Controllers/ProductsController.cs
@@ -0,0 +1,122 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using COMP003B.Assignment3.Models;
+
+namespace COMP003B.Assignment3.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private static List<Product> _products = new List<Product>();
+
+        // GET: ProductController
+        public ActionResult Index()
+        {
+            return View(_products);
+        }
+
+
+        // GET: ProductController/Create
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ProductController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Product product)
+        {
+          // TODO: add model stae validation
+          if (ModelState.IsValid)
+            {
+
+                if (!_products.Any(s => s.Id == product.Id))
+                {
+                    _products.Add(product);
+                }
+
+                return RedirectToAction(nameof(Index));
+                
+            }
+          return View();
+        }
+
+        // GET: ProductController/Edit/5
+        [HttpGet]
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var product = _products.FirstOrDefault(p => p.Id == id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return View(product);
+        }
+
+        // POST: ProductController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, Product product)
+        {
+            if (id != product.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                var existingProduct = _products.FirstOrDefault(p => p.Id == product.Id);
+
+                if (existingProduct != null)
+                {
+                    existingProduct.Name = product.Name;
+                    existingProduct.Price = product.Price;
+                }
+
+                return View(product);
+            }
+        }
+
+        // GET: ProductController/Delete/5
+        [HttpGet]
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var product = _products.FirstOrDefault(p => p.Id == id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return View(product);
+        }
+
+        // POST: ProductController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            var product = _products.FirstOrDefault(p => p.Id == id);
+
+            if (product != null)
+            {
+                _products.Remove(product);
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
