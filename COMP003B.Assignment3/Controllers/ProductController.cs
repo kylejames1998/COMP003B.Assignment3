eeeef5ca03f8f77da9af73946f673000bee3284c
Kyle James <140095432+kylejames1998@users.noreply.github.com>
2024-04-09 08:17:43 -0700

    Add project files.

---
+++ COMP003B.Assignment3/Controllers/ProductController.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using COMP003B.Assignment3.Models;
+
+namespace COMP003B.Assignment3.Controllers
+{
+    public class ProductController : Controller
+    {
+        private static List<ProductController> _products = new List<ProductController>();
+
+        // GET: ProductController
+        public ActionResult Index()
+        {
+            return View(_products);
+        }
+
+
+        // GET: ProductController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ProductController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(ProductController product)
+        {
+          // TODO: add model stae validation
+          if (ModelState.IsValid)
+            {
+
+                if (!_products.Any(p => p.Id == product.Id))
+                {
+                    product.Add(product);
+                }
+
+                return RedirectToAction(nameof(Index));
+                
+            }
+          return View();
+        }
+
+        // GET: ProductController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: ProductController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ProductController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: ProductController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ COMP003B.Assignment3/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace COMP003B.Assignment3.Models
+{
+    public class Product
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(120)]
+        [Display(Name = "Product Name")]
+        public string Name { get; set; }
+        [Required]
+        [Range(0, 100000)]
+        public decimal Price { get; set; }
+    }
+}
